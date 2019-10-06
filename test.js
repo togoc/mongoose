ee9fb3b4e237f8589fd96c4448fb915eb05d5fb6
togoc <49116563+togoc@users.noreply.github.com>
2019-10-07 03:39:21 +0800

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-var reg = /\W/;
- 
-var val = `我`
-
-console.log(reg.test(val), val.match(reg))
-
-// var reg = /(.*)(.*)/
-// var str = "baodu taobao"
-// console.log(reg.test(str), RegExp.$1, RegExp.$2) //true "baodu" "taobao"
