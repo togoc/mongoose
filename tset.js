8012d7344b36cf4a904bf3094f1c30d2f7b28c8d
togoc <49116563+togoc@users.noreply.github.com>
2019-10-10 16:28:49 +0800

    Create tset.js

---
+++ tset.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+
+// Build the connection string
+var dbURI = 'mongodb://localhost/students';
+const options = {
+    useNewUrlParser: true //使用客户端
+    , useUnifiedTopology: true //报错
+    , authSource: "admin",
+    user: "togoc",
+    pass: "tgc.123"
+}
+// ,
+//     user: "togoc",
+//     pass: "tgc.123",
+
+// Create the database connection
+mongoose.connect(dbURI, options);
+
+
+
+students.find(function (err, res) {
+   console.log(res)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CONNECTION EVENTS
+// When successfully connected
+db.on('connected', function () {
+    console.log('Mongoose 开始连接 ' + dbURI);
+});
+
+// If the connection throws an error
+db.on('error', function (err) {
+    console.log('Mongoose 连接错误 : ' + err);
+});
+// db.once("open", function () {
+//     console.log("Mongoose 集合打开成功!")
+// })
+db.once("close", function () {
+    console.log('Mongoose 断开连接')
+})
