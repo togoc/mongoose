e82b5219394442440e0511355091527157de261e
togoc <49116563+togoc@users.noreply.github.com>
2019-10-10 16:30:28 +0800

    Update tset.js

--- tset.js
+++ tset.js
@@ -19,9 +19,60 @@ const options = {
 mongoose.connect(dbURI, options);
 
 
+var mongoose = require("./db")
 
+var Schema = mongoose.Schema
+
+//创建集合
+var studentsSchema = Schema({
+    name: {
+        type: String,
+        default: "佚名"
+    },
+    age: {
+        type: Number,
+        default: 18
+    },
+    gender: {
+        type: String,
+        default: "男"
+    },
+    education: {
+        type: String,
+        default: "大学本科"
+    },
+    school: {
+        type: String,
+        default: "桂林电子科技大学"
+    },
+    zzmm: {
+        type: String,
+        default: "团员"
+    },
+    jod: {
+        type: String,
+        default: "前端开发"
+    },
+    native_place: {
+        type: String,
+        default: "广西壮族自治区"
+    },
+    phone: {
+        type: String,
+        default: "0771"
+    },
+    pic: {
+        type: String,
+        default: "/static/images/file.gif"
+    }
+})
+// studentSchema.methods.speak = function () {
+//     let name = this.name ? "my name is " + this.name : "i don`t have a name."
+//     console.log(name)
+// }
+mongoose.model("students", studentsSchema)
 students.find(function (err, res) {
-   console.log(res)
+    console.log(res)
 })
 
 
