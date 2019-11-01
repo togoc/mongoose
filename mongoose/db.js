6c81a9d3ba15fd07c0a2421d3d9e708d9f772da9
togoc <49116563+togoc@users.noreply.github.com>
2019-11-01 15:55:15 +0800

    update

--- mongoose/db.js
+++ mongoose/db.js
@@ -3,12 +3,15 @@ const db = mongoose.connection;
 
 
 // Build the connection string
-var dbURI = 'mongodb://182.254.195.126:27017/students';
+// var dbURI = 'mongodb://182.254.195.126:27017/students';
+var dbURI = 'mongodb://localhost:27017/students';
 const options = {
     useNewUrlParser: true //使用客户端
-    , useUnifiedTopology: true //报错
-    , authSource: "admin",
-    user: "togoc",
+        ,
+    useUnifiedTopology: true //报错
+        ,
+    authSource: "admin",
+    user: "root",
     pass: "tgc.123"
 }
 
@@ -17,18 +20,18 @@ mongoose.connect(dbURI, options);
 
 // CONNECTION EVENTS
 // When successfully connected
-db.on('connected', function () {
+db.on('connected', function() {
     console.log('Mongoose 开始连接 ' + dbURI);
 });
 
 // If the connection throws an error
-db.on('error', function (err) {
+db.on('error', function(err) {
     console.log('Mongoose 连接错误 : ' + err);
 });
 // db.once("open", function () {
 //     console.log("Mongoose 集合打开成功!")
 // })
-db.once("close", function () {
+db.once("close", function() {
     console.log('Mongoose 断开连接')
 })
 
